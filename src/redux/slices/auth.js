6540f40d5956314fb37bb103f7ba1dad1ac80560
Popenkov Anton <popenkovaa@yandex.ru>
2022-08-10 19:11:03 +0300

    check authorisation

--- src/redux/slices/auth.js
+++ src/redux/slices/auth.js
@@ -2,13 +2,11 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from '../../axios.js';
 
 //params - данные из формы авторизации
-export const fetchUserData = createAsyncThunk(
-  'auth/fetchUserData',
-  async (params) => {
-    const { data } = await axios.post('/auth/login', params);
-    return data;
-  }
-);
+// если данные верные, и пользователь есть в бд, бэкэнд вернет ответ, по которому мы поймем, что пользователь авторизован
+export const fetchAuth = createAsyncThunk('auth/fetchAuth', async (params) => {
+  const { data } = await axios.post('/auth/login', params);
+  return data;
+});
 
 const initialState = {
   data: null,
@@ -20,19 +18,21 @@ const authSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: {
-    [fetchUserData.pending]: (state) => {
+    [fetchAuth.pending]: (state) => {
       state.data = null;
       state.status = 'loading';
     },
-    [fetchUserData.fulfilled]: (state, action) => {
+    [fetchAuth.fulfilled]: (state, action) => {
       state.data = action.payload;
       state.status = 'loaded';
     },
-    [fetchUserData.rejected]: (state) => {
+    [fetchAuth.rejected]: (state) => {
       state.data = null;
       state.status = 'error';
     },
   },
 });
+
+export const isAuthedSelector = (state) => Boolean(state.auth.data);
 
 export const authReducer = authSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { authReducer } from './slices/auth';
 import { postReducer } from './slices/posts';
 
 const store = configureStore({
   reducer: {
     posts: postReducer,
+    auth: authReducer,
   },
 });
 
