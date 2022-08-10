b0441ef70059ccdc03c5cf7ebcd369937b6b3afc
Popenkov Anton <popenkovaa@yandex.ru>
2022-08-10 14:10:31 +0300

    posts added

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import CssBaseline from "@mui/material/CssBaseline";
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import CssBaseline from '@mui/material/CssBaseline';
 
-import "./index.scss";
-import { ThemeProvider } from "@mui/material";
-import { theme } from "./theme";
+import './index.scss';
+import { ThemeProvider } from '@mui/material';
+import { theme } from './theme';
+import store from './redux/store.js';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
+  <>
     <CssBaseline />
     <ThemeProvider theme={theme}>
-      <App />
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
     </ThemeProvider>
-  </React.StrictMode>
+  </>
 );
